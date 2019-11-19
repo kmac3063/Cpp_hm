ebd7339738179730f2233cdce356e76603086e13
kmac3063 <lantcov.iiu@students.dvfu.ru>
2019-11-19 10:23:35 +1000

    little change

--- hm_2/hash-map-kmac3063/main.cpp
+++ hm_2/hash-map-kmac3063/main.cpp
@@ -1,7 +1,13 @@
 #include "hash_map.hpp"
+#include <iostream>
 
 int main() {
-    fefu::hash_map MAPA;
-
+    fefu::hash_map<int, int> M(10);
+   /* M[2] = 3;
+    M[100] = 40;
+    std::cout << M[1] << '\n';
+    std::cout << M[2] << '\n';
+    std::cout << M[10] << '\n';
+    std::cout << M[100] << '\n';*/
     return 0;
 }
