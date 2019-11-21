e811ad1750fb176382ceb3bc50cbbb3f4427bcfc
kmac3063 <lantcov.iiu@students.dvfu.ru>
2019-11-22 00:01:04 +1000

    Написал пару функций

--- hm_2/hash-map-kmac3063/main.cpp
+++ hm_2/hash-map-kmac3063/main.cpp
@@ -12,19 +12,8 @@ int main() {
         M[i] = i + 10;
     }
 
-    auto it1 = M.begin();
-    int a1 = it1->first, a2 = it1->second;
-    
-    auto it2 = ++it1;
-    a1 = it2->first, a2 = it2->second;
-    
-    auto it3 = ++it2;
-    a1 = it3->first, a2 = it3->second;
-
-    int i = 0;
-    for (auto it = M.begin(); it != M.end(); it++) {
-        cout << it->first << " " << it->second << '\n';
-        i++;
+    for (auto it = M.begin(); it != M.end(); ++it) {
+        std::cout << it->first << " " << it->second << "\n";
     }
 
     return 0;
